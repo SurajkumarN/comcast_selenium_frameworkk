bfd5dd9ef7e1859d89a522196bd7c503e538bb76
SurajkumarN <sjn6724@gmail.com>
2022-03-09 20:14:25 +0530

    1

---
+++ src/main/java/com/crm/comcast/GenericLibrary/Date.java
@@ -0,0 +1,14 @@
+package com.crm.comcast.GenericLibrary;
+
+import org.testng.annotations.Test;
+
+public class Date {
+	@Test
+	public void date() {
+		java.util.Date d=new java.util.Date();
+		//d.toString();
+		System.out.println(d);
+		
+	}
+
+}
